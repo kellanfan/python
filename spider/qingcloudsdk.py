e0f9783d49e902d9991ed9d6882ad034775223fd
kellanfan <icyfk1989@163.com>
2017-06-13 11:33:12 +0800

    add for to get info

---
+++ spider/qingcloudsdk.py
@@ -0,0 +1,39 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Fri 09 Jun 2017 01:57:21 PM CST
+
+# File Name: qingcloudsdk.py
+# Description:
+
+"""
+import qingcloud.iaas
+import json
+conn = qingcloud.iaas.connect_to_zone(
+        'pek3a',
+        'JKEDNDMVPAFWGXFTEAXL',
+        'zUD7r8f1X8R28MHgKNOWUCm0NWD0WucyZv1H8aAg'
+        )
+
+def get_loadbalancer_id():
+    ret = conn.describe_loadbalancers(status = ['active'])
+    loadbalancer_id = []
+    for lb in ret['loadbalancer_set']:
+        loadbalancer_id.append(lb['loadbalancer_id'])
+    return loadbalancer_id
+
+def get_loadbalancer_status():
+    lbid_list = get_loadbalancer_id()
+    result = []
+    result_dict = {}
+    for lbid in lbid_list:
+        ret = conn.describe_loadbalancer_backends(loadbalancer_id = lbid)
+        a = ret['loadbalancer_backend_set'][0]
+        result_dict['loadbalancer_id'] = a['loadbalancer_id']
+        result_dict['loadbalancer_backend_id'] = a['loadbalancer_backend_id']
+        result_dict['status'] = a['status']
+        result.append(result_dict)
+    return result
+status = get_loadbalancer_status()
+print status
