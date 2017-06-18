80f9e3623c72a61b3b8150f3bfcef54d5681836c
kellanfan <icyfk1989@163.com>
2017-06-18 09:31:23 +0800

    OOP duotai

--- test/classh.py
+++ test/classh.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+#coding=utf-8
 
-class Student(object):
+class School(object):
+    def __init__(self, sname):
+        self.sname = sname
+    def ps(self):
+        print "i\'m in the %s school" %self.sname
 
-    def __init__(self, name, score):
+class Student(School):
+
+    def __init__(self, sname, name, score):
         self.__name = name
         self.__score = score
-
+        School.__init__(self,sname)
     def print_score(self):
         print '%s: %s' % (self.__name, self.__score)
 
@@ -19,3 +26,19 @@ class Student(object):
             self.__score = score
         else:
             raise ValueError('bad score')
+    def ps1(self):
+        print "I\'m in %s , and my name is %s" %(self.sname, self.__name)
+
+class Teacher(object):
+    def ps(self):
+        print "i\'m not in School class and Student Class,but i have ps"
+    def ps1(self):
+        print "i\'m not in School class and Student Class,but i have ps1"
+
+def f(x):
+    x.ps()
+    x.ps1()
+
+
+f(Student('hengshui','kellan','100'))
+f(Teacher())
