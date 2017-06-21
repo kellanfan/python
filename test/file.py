e9aa732ebb1a83cf903ba6dcf4e1279855983c34
kellanfan <icyfk1989@163.com>
2017-06-21 19:40:13 +0800

    addaa

--- test/file.py
+++ test/file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-f = file('/pitrix/python/test/data.txt')
+f = file('/root/python/test/data.txt')
 data = f.read()
 print data
 f.close()
 print "===="
-g = file('/pitrix/python/test/data.txt')
+g = file('/root/python/test/data.txt')
 data1 = g.readline()
 print data1
 print "===="
-h = file('/pitrix/python/test/data.txt')
+h = file('/root/python/test/data.txt')
 data2 = h.readlines()
 print data2
 
