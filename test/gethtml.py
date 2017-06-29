fed752dbc8eb8dc69c6c1963a0a548886d4ca15c
kellanfan <icyfk1989@163.com>
2017-06-29 13:45:29 +0800

    add header

--- test/gethtml.py
+++ test/gethtml.py
@@ -2,11 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 import urllib2
-
+def getreq(url):
+    header = {
+        'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64)'
+                       'AppleWebKit/537.36 (KHTML, like Gecko)'
+                       'Chrome/56.0.2924.87 Safari/537.36'),
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    }
+    req = urllib2.Request(url, headers = header)
+    return req
 def gethtml(url):
     page = urllib2.urlopen(url)
     html = page.read()
     return html
 
 url = raw_input("your url: ")
-print gethtml(url)
+req = getreq(url)
+print gethtml(req)
