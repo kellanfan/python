187ca150001201de6b1b8614366bd3483d995937
kellanfan <icyfk1989@163.com>
2017-07-05 15:08:15 +0800

    take api

---
+++ spider/geturl.py
@@ -0,0 +1,38 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Thu 08 Jun 2017 09:06:25 AM CST
+
+# File Name: geturl.py
+# Description:
+
+"""
+import urllib2, urllib, json, datetime
+import base64, hmac
+from hashlib import sha256
+from key import value
+
+def geturl(data, zone, access_key_id, secret_access_key):
+    url = 'https://api.qingcloud.com/iaas/'
+    now = datetime.datetime.now()
+    time = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+    data['time_stamp'] = time
+    data['access_key_id'] = access_key_id
+    data['zone'] = zone
+    keys = sorted(data.keys())
+    string_to_sign = 'GET\n/iaas/\n'
+    pairs = []
+    for key in keys:
+        val = data[key].encode('utf-8')
+        pairs.append(urllib.quote(key, safe='') + '=' +
+                     urllib.quote(val, safe='-_~'))
+    qs = '&'.join(pairs)
+    string_to_sign += qs
+    data = urllib.urlencode(data)
+    h = hmac.new(secret_access_key, digestmod=sha256)
+    h.update(string_to_sign)
+    sign = base64.b64encode(h.digest()).strip()
+    signature = urllib.quote_plus(sign)
+    url2 = url + '?' + data + '&signature=' + signature
+    return url2
---
+++ spider/test.py
@@ -0,0 +1,26 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Wed 05 Jul 2017 02:59:09 PM CST
+
+# File Name: test.py
+# Description:
+
+"""
+import urllib2
+from geturl import geturl
+from key import value
+
+secret_access_key = value.get('secret_access_key')
+access_key_id = value.get('access_key_id')
+zone = 'pek3a'
+data={'action':'DescribeInstances',
+      'version':'1',
+      'signature_method':'HmacSHA256',
+      'signature_version':'1',
+     }
+url =  geturl(data, zone, access_key_id, secret_access_key)
+response = urllib2.urlopen(url)
+apicontent = response.read()
+print apicontent
