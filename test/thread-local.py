a0ad07eac1536eb47b4df7a39ca113dfc08d1102
kellanfan <icyfk1989@163.com>
2017-07-26 18:44:07 +0800

    threadlocal shuoming

--- test/thread-local.py
+++ test/thread-local.py
@@ -5,7 +5,11 @@
 # Created Time : Wed 26 Jul 2017 06:41:45 PM CST
 
 # File Name: thread-local.py
-# Description:
+# Description: 全局变量local_school就是一个ThreadLocal对象，每个Thread对它都可以读写student属性，但互不影响。你可以把local_school看成全局变量，但每个属性如local_school.student都是线程的局部变量，可以任意读写而互不干扰，也不用管理锁的问题，ThreadLocal内部会处理。
+
+可以理解为全局变量local_school是一个dict，不但可以用local_school.student，还可以绑定其他变量，如local_school.teacher等等。
+
+ThreadLocal最常用的地方就是为每个线程绑定一个数据库连接，HTTP请求，用户身份信息等，这样一个线程的所有调用到的处理函数都可以非常方便地访问这些资源。
 
 """
 
