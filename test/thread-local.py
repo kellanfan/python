42c1cd7b3729bca340eb9a525377554a7356af22
kellanfan <icyfk1989@163.com>
2017-07-26 18:42:13 +0800

    threadlocal

---
+++ test/thread-local.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Wed 26 Jul 2017 06:41:45 PM CST
+
+# File Name: thread-local.py
+# Description:
+
+"""
+
+import threading
+
+# 创建全局ThreadLocal对象:
+local_school = threading.local()
+
+def process_student():
+    print 'Hello, %s (in %s)' % (local_school.student, threading.current_thread().name)
+
+def process_thread(name):
+    # 绑定ThreadLocal的student:
+    local_school.student = name
+    process_student()
+
+t1 = threading.Thread(target= process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target= process_thread, args=('Bob',), name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
