f3f2fc5ccac64ed9d196f6238c405075835773d9
kellanfan <icyfk1989@163.com>
2017-07-31 19:35:44 +0800

    re

---
+++ test/re_test.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Mon 31 Jul 2017 07:35:19 PM CST
+
+# File Name: re_test.py
+# Description:
+
+"""
+
+import re
+
+print('Test: 010-12345')
+m = re.match(r'^(\d{3})-(\d{3,8})$', '010-12345')
+print(m.group(1), m.group(2))
+
+t = '19:05:30'
+print('Test:', t)
+m = re.match(r'^(0[0-9]|1[0-9]|2[0-3]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])$', t)
+print(m.groups())
