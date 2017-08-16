e7a7646f2d28ef8b6695ad58d832fcc2ec3d28e6
kellanfan <icyfk1989@163.com>
2017-08-16 10:25:38 +0800

    diedai itertools

---
+++ test/bulidin-diedai.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+"""
+# Author: kellanfan
+# Created Time : Wed 16 Aug 2017 09:34:17 AM CST
+
+# File Name: bulidin-diedai.py
+# Description:
+
+"""
+
+import itertools
+a = itertools.count(1)
+for i in a:
+    print i
