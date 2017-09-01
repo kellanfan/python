ca07ec84bb575cb4cdb554d5ce1547d3eea08f89
kellanfan <icyfk1989@163.com>
2017-09-01 19:29:03 +0800

    重新整理代码并加注释

--- login-system.py
+++ login-system.py
@@ -11,33 +11,33 @@
 
 import hashlib
 import json
-
+#输入用户名密码
 def input_info():
     username = raw_input("please input your username: ")
     password = raw_input("please input your password: ")
     return username, password
-
+#注册后保存数据到json文件
 def save_data(db):
     try:
         l = read_data()
-        with open('db.json', 'w') as f:
+        with open('login-and-register.json', 'w') as f:
             l.append(db)
             json.dump(l, f)
     except IOError:
         print "save data failed..."
     finally:
         f.close()
-
+#从json文件中读取数据
 def read_data():
     try:
-        with open('db.json', 'r') as f:
+        with open('login-and-register.json', 'r') as f:
             content = json.load(f)
         f.close()
         return content
     except:
         content = []
         return content
-
+#获取用户名列表
 def get_user_list():
     datalist = read_data()
     had_usernamelist = []
@@ -46,7 +46,7 @@ def get_user_list():
         had_usernamelist.append(had_username)
     return had_usernamelist
 
-
+#注册过程
 def register(username, password):
     check_password = raw_input("please input your password again to ensure it. Your password: ")
     while True:
@@ -58,12 +58,12 @@ def register(username, password):
     db = {}
     db[username] = calc_md5(username + password + 'kellan-salt')
     save_data(db)
-    
+#md5编码    
 def calc_md5(string):
     md5 = hashlib.md5()
     md5.update(string)
     return md5.hexdigest()
-
+#登陆过程
 def login(username, password):
     had_username = get_user_list()
     data = read_data()
@@ -73,23 +73,16 @@ def login(username, password):
     userNameList = []
     for user in had_username:
         userNameList.append(user[0])
-
     if username in userNameList:
         md5_pass = calc_md5(username + password + 'kellan-salt')
         if users_dict[username] == md5_pass:
             return 0
         else:
             return 1
-        for i in range(5):
-            if users_dict[username] == md5_pass:
-                print "login successful! have fun!!!"
-                break
-            else:
-                print "Password is ERROR!!! Please try again!"
-                continue
     else:
         return 2
 
+#主程序
 
 if __name__ == '__main__':
     print "="*30
@@ -100,18 +93,14 @@ if __name__ == '__main__':
     if choose == 'login' or choose == '0':
         (username, password) = input_info()
         i = 0
-        for i < 5:
-            flag = login(username, password)
-            if flag == 0:
-                print "login successful! have fun!!!"
-                break
-            elif flag == 1:
-                print "Password is ERROR!!! Please try again!"
-
-            elif flag == 2:
-                print "NOT has the user, please register!!!"
-                
-                
+        flag = login(username, password)
+        if flag == 0:
+            print "login successful! have fun!!!"
+        elif flag == 1:
+#BUG:正常来说这应该有个循环，比如连续输错3次后再退出
+            print "Password is ERROR!!! Please try again!"
+        elif flag == 2:
+            print "NOT has the user, please register!!!"
     elif choose == 'register' or choose == '1':
         (username, password) = input_info()
         register(username, password)
