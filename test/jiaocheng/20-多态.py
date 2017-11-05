87661dcce11f0d485e10a1df4452299d8196303b
kellanfan <icyfk1989@163.com>
2017-11-05 15:11:30 +0800

    多态

---
+++ test/jiaocheng/20-多态.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Sun 05 Nov 2017 03:05:50 PM CST
+
+# File Name: 20-多态.py
+# Description: 面向对象的3大要素，封装、继承、多态
+
+"""
+
+class Dog(object):
+    def print_self(self):
+        print "大家好"
+class Xiaotq(Dog):
+    def print_self(self):
+        print "hello"
+
+def introduce(temp):
+    temp.print_self()
+dog1 = Dog()
+dog2 = Xiaotq()
+introduce(dog1)
+introduce(dog2)
