941e6b632bdbbe2ee9517dc73951636c2501b66f
kellanfan <icyfk1989@163.com>
2018-01-23 19:27:36 +0800

    property

---
+++ test/jiaocheng/01-python高级-1/05-property.py
@@ -0,0 +1,31 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Tue 23 Jan 2018 07:02:02 PM CST
+
+# File Name: 05-property.py
+# Description:
+
+"""
+class Test(object):
+    def __init__(self):
+        self.__num = 100
+
+    def setnum(self, new_num):
+        self.__num = new_num
+
+    def getnum(self):
+        return self.__num
+
+    num = property(getnum, setnum)
+
+t = Test()
+t.num = 9 #相当于调用了 t.setnum(9)
+print t.num #相当于调用了 t.getnum()
+
+#注意点:
+#t.num到底调用哪个，要根据实际场景来判断
+#1.如果是给t.num赋值，那么一定调用setnum()
+#2.如果是获取t.num的值，那么一定调用getnum()
+#property的作用：相当于把方法进行了封装，开发者在对属性设置数据的时候方便
