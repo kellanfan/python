ddc94202952904c8ecc932b523f374cfd41dabcf
kellanfan <icyfk1989@163.com>
2018-05-11 17:33:23 +0800

    解决报MaxTries的bug

--- spider/meijutiantang.py
+++ spider/meijutiantang.py
@@ -8,26 +8,17 @@
 # Description:
 
 """
-import requests
 import re
 import os
 import time
+import logging
+from openurl import OpenUrl
 
-def open_url(url):  
-    headers = {
-              'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64)'
-                               'AppleWebKit/537.36 (KHTML, like Gecko)'
-                               'Chrome/56.0.2924.87 Safari/537.36'),
-              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    }
-    response = requests.get(url, headers=headers)
-    #原页面为gb2312，需要指定requests对象encoding值
-    response.encoding = 'gb2312'
-    return response.status_code, response.text
 
-def save_text(url_id, link_list):
-    file_name = './meiju/%s.txt'%url_id
+def save_text(url_id, title, link_list):
+    file_name = '/root/data/meiju/%s.txt'%url_id
     f = open(file_name, 'w+')
+    f.write(title + '\n')
     for link in link_list:
         f.write(link + '\n')
     f.close()
@@ -36,16 +27,19 @@ def main():
     #构建url
     url_header = 'http://www.meijutt.com/content/meiju'
     url_end = '.html'
-    reg = re.compile(r'<a href="(ed2k://\|file\|.+?)">') #加？非贪婪
-    if not os.path.exists('./meiju'):
-        os.mkdir('./meiju')
+    text_reg = re.compile(r'<a href="(ed2k://\|file\|.+?)">') #加？非贪婪
+    
+    if not os.path.exists('/root/data/meiju'):
+        os.mkdir('/root/data/meiju')
 
-    for url_id in range(20000, 50000):
+    for url_id in range(10000, 40000):
         url = url_header + str(url_id)  + url_end
-        status, html = open_url(url)
+        ourl = OpenUrl(url, 'gb2312')
+        status, html = ourl.openurl()
         if status == 200:
-            link_list = re.findall(reg, html)
-            save_text(url_id, link_list)
+            link_list = re.findall(text_reg, html)
+            name = re.search('<title>(.+?)迅雷下载',html).group(1)
+            save_text(url_id, name, link_list)
         time.sleep(0.5)
 
 if __name__ == '__main__':
--- spider/openurl.py
+++ spider/openurl.py
@@ -10,19 +10,31 @@
 """
 
 import requests
-def open_url(url, encode):  
-    headers = {
+
+class OpenUrl(object):
+    def __init__(self, url, encode = 'utf-8'):
+        self.__url = url
+        self.__headers = {
               'user-agent': ('Mozilla/5.0 (Windows NT 6.2; WOW64)'
                                'AppleWebKit/537.36 (KHTML, like Gecko)'
                                'Chrome/56.0.2924.87 Safari/537.36'),
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    }
-    response = requests.get(url, headers=headers)
-    #原页面为gb2312，需要指定requests对象encoding值
-    response.encoding = encode
-    return response.status_code, response.text
+              'Connection': 'close'
+        }
+        self.__encode = encode
+
+    def openurl(self):
+        try:
+            requests.adapters.DEFAULT_RETRIES = 10
+            response = requests.get(self.__url, headers=self.__headers)
+            response.encoding = self.__encode
+            return response.status_code, response.text
+        except:
+            status_code = 400
+            text = None
+            return status_code, text
 
 if __name__ == '__main__':
-    #url = 'http://www.runningman-fan.com/180429-zz.html'
-    url = 'https://www.piaohua.com/html/kehuan/2018/0502/33625.html'
-    print(open_url(url,'utf-8'))
+    url = 'http://www.meijutt.com/content/meiju23030.html'
+    ourl = OpenUrl(url,'gb2312')
+    print(ourl.openurl())
