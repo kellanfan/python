0c9b77aa6c3b70f66122ad89ab4775463d9c7b5e
kellanfan <icyfk1989@163.com>
2018-05-25 17:04:47 +0800

    存储到数据库

--- spider/meijutiantang.py
+++ spider/meijutiantang.py
@@ -12,7 +12,8 @@ import re
 import os
 import time
 import logging
-from openurl import OpenUrl
+from misc import openurl
+from misc import mysql_connect
 
 def logger(code, msg):
     '''记录日志'''
@@ -32,28 +33,40 @@ def save_text(url_id, title, link_list):
         f.write(link + '\n')
     f.close()
     
+def send_mysql(name, str_down):
+    sql = "insert into meiju(name, content) value ('%s', '%s')"%(name,str_down)
+    connect = mysql_connect.MysqlConnect('./misc/mysql_data.yaml')
+    code = connect.change_data('spiderdata', sql)
+    if code == 0:
+        print('%s ok'%name)
+    else:
+        print('%s error'%name)
+
 def main():
     #构建url
     url_header = 'http://www.meijutt.com/content/meiju'
     url_end = '.html'
     text_reg = re.compile(r'<a href="(ed2k://\|file\|.+?)">') #加？非贪婪
     
-    if not os.path.exists('/root/data/meiju'):
-        os.mkdir('/root/data/meiju')
+    #if not os.path.exists('/root/data/meiju'):
+    #    os.mkdir('/root/data/meiju')
 
     for url_id in range(20000, 23600):
         url = url_header + str(url_id)  + url_end
-        ourl = OpenUrl(url, 'gb2312')
+        ourl = openurl.OpenUrl(url, 'gb2312')
         status, html = ourl.openurl()
         logger(status, url)
         if status == 200:
             link_list = re.findall(text_reg, html)
+            if link_list != []:
+                str_down = '#'.join(link_list)
+            
             try:
                 name = re.search('<title>(.+?)迅雷下载',html).group(1)
             except:
-                name = None
+                name = "None"
             else:
-                save_text(url_id, name, link_list)
+                send_mysql(name, str_down)
         time.sleep(0.5)
 
 if __name__ == '__main__':
