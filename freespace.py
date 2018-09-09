b947756ce7becbe453e8a33e94312074058fe84d
kellanfan <icyfk1989@163.com>
2018-09-09 23:31:20 +0800

    free space

---
+++ freespace.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Sun 09 Sep 2018 09:20:09 PM CST
+
+# File Name: freespace.py
+# Description:
+
+"""
+
+import os
+dirlist = ['/var/log', '/tmp']
+for dir in dirlist:
+    filelist = os.listdir(dir)
+    for i in range(0,len(filelist)):
+        path = os.path.join(dir,filelist[i])
+        if os.path.isfile(path):
+            if os.path.getsize(path) >= 10240000 and path.endswith('.log'):
+                os.remove(path)
+                #with open(path,'w') as f:
+                #    f.truncate()
+                print "clean ok..."
