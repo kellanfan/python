23ebd46a216e269fc331e22d5d763d4e96e06b19
kellanfan <icyfk1989@163.com>
2018-09-14 08:38:43 +0800

    完整版

--- spider/piaohua.py
+++ spider/piaohua.py
@@ -12,75 +12,91 @@
 import re
 import time
 import sys
+import redis
 #发现re确实不好处理一些特殊的html，改动bs4
 from bs4 import BeautifulSoup
 from lxml import etree
 from misc import mysql_connect
 from misc import openurl
 
-def get_url(ftype):
-    '''对于这种临时数据暂时存到文件中'''
-    main_url = 'https://www.piaohua.com/html/%s/index.html' %ftype
-    ourl = openurl.OpenUrl(main_url)
-    code,main_content = ourl.openurl()
-    if code ==  200:
-        #soup = BeautifulSoup(main_content, 'lxml')
-        #b = soup.find_all(text=re.compile("共(\d+)页"))[0]
-        #pages = re.sub('\D', "", str(b.split('页')[0]))
-        selecter = etree.HTML(main_content)
-        pages = int(selecter.xpath('//li[@class="end"]/a')[0].attrib['href'].split("_")[1].split('.')[0])
-    else:
-        print("bad url: %s" %main_url)
-        sys.exit(-1)
-    fname = 'url_list_' + ftype +'.txt'
-    f = open(fname,'w')
-    for page in range(1,int(pages)):
-        list_url = 'https://www.piaohua.com/html/%s/list_%d.html' %(ftype, page)
-        sub_ourl = openurl.OpenUrl(list_url)
-        sub_code,sub_content = sub_ourl.openurl()
-        if sub_code == 200:
-            selector = etree.HTML(sub_content)
-            for link in selector.xpath('//span/a'):
-                sub_url = link.attrib['href']
-                if sub_url.startswith('/html/'+ftype ):
-                    f.write(sub_url+'\n')
-        time.sleep(0.5)
-    f.close()
-    time.sleep(1)
-    return f
+class Piaohua(object):
+    def __init__(self,ftype):
+        self.__ftype = ftype
+        self.__redis_link = self.__redis_connect()
 
-def get_download_url(filename,ftype):
-    '''主要函数'''
-    #读取文件
-    with open(filename) as f:
-        for line in f.readlines():
-            #读取文件每一行
-            line = line.split('\n')[0]
-            #构建url
-            url = 'https://www.piaohua.com' + line
-            #获取html内容
-            ourl = openurl.OpenUrl(url)
-            code, content = ourl.openurl()
-            #初始化list
-            list_down = []
-            #判断是否正确打开
-            if code == 200:
-                #反爬虫
-                time.sleep(0.5)
-                #构建soup
-                soup = BeautifulSoup(content,'lxml')
-                #获取名称
-                name = soup.title.string.split('_')[0]
-                #获取a标签的href属性，并去除\r，避免后续处理的麻烦
-                for link in soup.find_all('a'):
-                    url = link.get('href')
-                    if 'ftp' in url:
-                        url = ''.join(url.split())
-                        list_down.append(url)
-                #构建最后的str
-                if list_down != []:
-                    str_down = '#'.join(list_down)
-                    send_mysql(name, str_down, ftype)
+    def __redis_connect(self):
+        pool = redis.ConnectionPool(host='127.0.0.1',port=6379)
+        return redis.Redis(connection_pool=pool)
+
+    def get_url(self):
+        main_url = 'https://www.piaohua.com/html/%s/index.html' %self.__ftype
+        ourl = openurl.OpenUrl(main_url)
+        code,main_content = ourl.openurl()
+        if code ==  200:
+            #soup = BeautifulSoup(main_content, 'lxml')
+            #b = soup.find_all(text=re.compile("共(\d+)页"))[0]
+            #pages = re.sub('\D', "", str(b.split('页')[0]))
+            selecter = etree.HTML(main_content)
+            pages = int(selecter.xpath('//li[@class="end"]/a')[0].attrib['href'].split("_")[1].split('.')[0])
+        else:
+            print("bad url: %s" %main_url)
+            sys.exit(-1)
+        redis_id = 0
+        for page in range(1,int(pages)):
+            list_url = 'https://www.piaohua.com/html/%s/list_%d.html' %(self.__ftype, page)
+            sub_ourl = openurl.OpenUrl(list_url)
+            sub_code,sub_content = sub_ourl.openurl()
+            if sub_code == 200:
+                selector = etree.HTML(sub_content)
+                for link in selector.xpath('//span/a'):
+                    sub_url = link.attrib['href']
+                    if sub_url.startswith('/html/'+ self.__ftype ):
+                        fkey = self.__ftype + str(redis_id)
+                        self.__redis_link.set(fkey, sub_url, ex=21600)
+                        redis_id += 1
+            time.sleep(0.5)
+
+    def get_download_url(self):
+        '''主要函数'''
+        redis_id = 0
+        while True:
+            fkey = self.__ftype + str(redis_id)
+            line = self.__redis_link.get(fkey)
+            redis_id += 1
+            if line:
+                 #构建url
+                 url = 'https://www.piaohua.com' + line.decode()
+                 #获取html内容
+                 ourl = openurl.OpenUrl(url)
+                 code, content = ourl.openurl()
+                 #初始化list
+                 list_down = []
+                 #判断是否正确打开
+                 if code == 200:
+                     #反爬虫
+                     time.sleep(0.5)
+                     #构建soup
+                     soup = BeautifulSoup(content,'lxml')
+                     #获取名称
+                     name = soup.title.string.split('_')[0]
+                     #获取a标签的href属性，并去除\r，避免后续处理的麻烦
+                     for link in soup.find_all('a'):
+                         url = link.get('href')
+                         if not url is None and 'ftp' in url:
+                            url = ''.join(url.split())
+                            list_down.append(url)
+                         else:
+                            continue
+                     #构建最后的str
+                     if list_down != []:
+                         str_down = '#'.join(list_down)
+                         send_mysql(name, str_down, self.__ftype)
+                     else:
+                         print("can not find dowload link...")
+                 else:
+                     print("bad url: %s" %url)
+            else:
+                break
 
 def send_mysql(name, str_down, ftype):
     '''将数据写入数据库'''
@@ -95,5 +111,6 @@ def send_mysql(name, str_down, ftype):
 
 if __name__ == '__main__':
     ftype = input("需要下载的类型:\n<dongzuo,xiju,aiqing,kehuan,juqing,xuannian,wenyi,zhanzheng,kongbu,zainan,lianxuju,dongman>\n:")
-    f = get_url(ftype)
-    get_download_url(f,ftype)
+    piaohua = Piaohua(ftype)
+    piaohua.get_url()
+    piaohua.get_download_url()
