ce12acd3217c6ac5945e2e897bedde9813412b5e
kellanfan <icyfk1989@163.com>
2018-09-19 22:00:48 +0800

    algorithm

---
+++ algorithm/Two_fork_pile.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Wed 12 Sep 2018 08:04:53 AM CST
+
+# File Name: Two_fork_pile.py
+# Description: 通过python实现二叉堆
+
+"""
+
+
+class TwoForkPile(object):
+    def __init__(self,array):
+        self.__array = array
+        self.__lenth = len(array)
+
+    def __upitem(self,children_index):
+        parent_index = ()
+
+        
+
+
