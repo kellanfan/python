3a7f292cf91218dd8037449a26023b14c689be14
kellanfan <icyfk1989@163.com>
2019-02-11 09:10:37 +0800

    优化脚本

--- spider/hanfan.py
+++ spider/hanfan.py
@@ -21,24 +21,24 @@ class Hanfan(object):
         mylog = Logger(os.path.join(os.path.abspath(os.path.curdir),'misc/spider_log.yaml'))
         self.__logger = mylog.outputLog()
         self.mysql_connect = mysql_connect.MysqlConnect(os.path.join(os.path.abspath(os.path.curdir),'misc/mysql_data.yaml'))
+        self.main_url = 'http://www.hanfan.cc/'
 
     def __redis_connect(self):
         pool = redis.ConnectionPool(host='127.0.0.1',port=6379)
         return redis.Redis(connection_pool=pool)
     
-    def get_url(self):
-        main_url = 'http://www.hanfan.cc/movie/'
-        ourl = OpenUrl(main_url)
+    def get_url(self, ftype):
+        ourl = OpenUrl(self.main_url + ftype)
         code,main_content = ourl.openurl()
         if code == 200:
             selecter = etree.HTML(main_content)
             pages = int(selecter.xpath('/html/body/section/div/div/div[3]/ul/li[8]/span/text()')[0].split(' ')[1])
         else:
-            print("bad url: %s" %main_url)
+            print("bad url: %s" %self.main_url)
             sys.exit()
 
         for page in range(1,pages):
-            page_url = 'http://www.hanfan.cc/movie/page/%s/'%page
+            page_url = self.main_url + ftype + '/page/%s/'%page
             sub_ourl = OpenUrl(page_url)
             sub_code,sub_content = sub_ourl.openurl()
             if sub_code == 200:
@@ -48,6 +48,10 @@ class Hanfan(object):
                     name = link.text
                     sub_url = link.attrib['href'] + '#prettyPhoto/0/'
                     self.__redis_link.set(name, sub_url, ex=21600)
+            else:
+                self.__logger.error('[%s] can not open...'%page_url)
+                continue
+
             time.sleep(1)
                     
     def get_download_url(self):
@@ -67,28 +71,28 @@ class Hanfan(object):
                         cloudpan_url = '|'.join(cloudpan_url)
                         cloudpan_pass = '|'.join(selecter.xpath('//div[@class="part"]/text()')[2:4])
                     else:
-                        self.__logger.error('[%s] donot has cloudpan download link...'%fkey)
+                        self.__logger.error('[%s] donot has cloudpan download link...'%fkey.decode())
                         continue
                 except:
-                    self.__logger.error('[%s] miss something..'%fkey)
+                    self.__logger.error('[%s] miss something..'%fkey.decode())
                     continue
                 self.send_mysql(fkey, cloudpan_url, cloudpan_pass)
             else:
-                self.__logger.error('[%s] can not open the download page..'%fkey)
+                self.__logger.error('[%s] can not open the download page..'%fkey.decode())
                 continue
             time.sleep(0.5)
 
     def send_mysql(self, name, cloudpan_url, cloudpan_pass):
         '''将数据写入数据库'''
         sql = "insert into hanfan(name, url, panpass) value ('%s', '%s', '%s')" %(name.decode(),cloudpan_url,cloudpan_pass)
-        print(sql)
         code = self.mysql_connect.change_data('spiderdata', sql)
         if code == 0:
-            self.__logger.info('[%s] ok'%name)
+            self.__logger.info('[%s] ok'%name.decode())
         else:
-            self.__logger.error('[%s] error,message: [%s]'%(name, code))
+            self.__logger.error('[%s] error,message: [%s]'%(name.decode(), code))
 
 if __name__ == '__main__':
     a = Hanfan()
-    #a.get_url()
+    for i in ['variety', 'movie', 'hanju']:
+        a.get_url(i)
     a.get_download_url()
