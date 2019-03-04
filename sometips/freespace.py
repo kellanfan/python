4fe31cd64a3800571844b14757b733b48a136a0f
kellanfan <icyfk1989@163.com>
2019-03-04 14:27:25 +0800

    优化代码

--- sometips/freespace.py
+++ sometips/freespace.py
@@ -10,12 +10,36 @@
 """
 
 import os
-dirlist = ['/var/log', '/tmp']
-for dir in dirlist:
-    filelist = os.listdir(dir)
-    for i in range(0,len(filelist)):
-        path = os.path.join(dir,filelist[i])
-        if os.path.isfile(path):
-            if os.path.getsize(path) >= 10240000 and path.endswith('.log'):
-                os.remove(path)
-                print("clean ok...")
+
+def get_files(path):
+    file_list = []
+    for root,dirs,files in os.walk(path):
+        for name in files:
+            file_list.append(os.path.join(root, name))
+    return file_list
+
+def clean_file(item):
+    file_size = os.path.getsize(item)
+    if file_size >= 10240000 and item.endswith('.log'):
+        with open(item,'w') as f:
+            step = 20480
+            while file_size > 0:
+                f.seek(0)     
+                f.truncate(step)
+                file_size -= step
+
+        print('[%s] has been clean..'%item)
+
+def main():
+    files_list = []
+    dirlist = ['/var/log', '/tmp']
+    for dirs in dirlist:
+        files_list.extend(get_files(dirs))
+
+    for files in files_list:
+        clean_file(files)
+
+    print("clean files done..")
+
+if __name__ == '__main__':
+    main()
