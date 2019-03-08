6efec23cedd3536d370f39987aa4e9bb20bdaab2
kellanfan <icyfk1989@163.com>
2019-03-08 10:09:35 +0800

    检查网络中哪个ip在线

---
+++ sometips/who_reached.py
@@ -0,0 +1,27 @@
+#/usr/bin/env python
+#coding=utf8
+"""
+# Author: kellanfan
+# Created Time : Fri 08 Mar 2019 09:31:18 AM CST
+
+# File Name: netmask-counter.py
+# Description:
+
+"""
+import sys
+from IPy import IP
+from pings import Ping
+
+ip = input('请输入ip信息<10.1.1.0/24>: ')
+
+try:
+    IP(ip).net()
+    IP(ip).netmask()
+except:
+    print("您输入的ip地址不合法，请重新输入！")
+    sys.exit()
+
+mypings = Ping(timeout=3)
+for i in IP(ip):
+    if mypings.ping(str(i)).is_reached():
+        print("%s is reached.."%i)
