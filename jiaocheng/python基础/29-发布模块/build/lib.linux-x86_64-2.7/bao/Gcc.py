8ac0460621aa01d530ef902c9337640234a734df
kellanfan <icyfk1989@163.com>
2019-03-10 20:54:38 +0800

    清除无用文件

--- jiaocheng/python基础/29-发布模块/build/lib.linux-x86_64-2.7/bao/Gcc.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
- 
-def Gcc():
-   print "I'm Gcc Phone"
--- jiaocheng/python基础/29-发布模块/build/lib.linux-x86_64-2.7/bao/Pots.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
- 
-def Pots():
-   print "I'm Pots Phone"
