f2d8df7e19c2068f0b4ec3c346c53dab2bed28b9
kellanfan <icyfk1989@163.com>
2019-08-02 16:50:29 +0800

    改为py3

--- test/01-从1加到指定数字.py
+++ test/01-从1加到指定数字.py
@@ -1,4 +1,16 @@
-#!/usr/bin/python
+# pylint: disable=no-member
+# -*- encoding: utf-8 -*-
+'''
+@File    :   01-从1加到指定数字.py
+@Time    :   2019/08/02 16:48:05
+@Author  :   Kellan Fan 
+@Version :   1.0
+@Contact :   kellanfan1989@gmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+
 import sys
 def addto(num):
 	s = 0
@@ -9,9 +21,9 @@ def addto(num):
 def main():
 	try:
 		num = input('what num you want add to: ')
-		print "the sum is %s" %addto(num)
+		print("the sum is %s" %addto(num))
 	except:
-		print "please input number!"
+		print("please input number!")
 		sys.exit()
 
 if __name__ == '__main__':
--- test/05-输出三角形.py
+++ test/05-输出三角形.py
@@ -1,6 +1,18 @@
-#!/usr/bin/python
+# pylint: disable=no-member
+# -*- encoding: utf-8 -*-
+'''
+@File    :   05-输出三角形.py
+@Time    :   2019/08/02 16:47:31
+@Author  :   Kellan Fan 
+@Version :   1.0
+@Contact :   kellanfan1989@gmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+
 for i in range(1,4):
     for j in range(1,i+1):
-        print "*",
-    print
+        print("*",end='')
+    print()
 
