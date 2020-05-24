6f9f4380950f188cf09edc13fa968d60ba75b480
kellanfan <icyfk1989@163.com>
2020-05-24 11:00:24 +0800

    更新代码，适应容器模式

--- spiderman/meiju.py
+++ spiderman/meiju.py
@@ -59,33 +59,37 @@ def page_link(url):
 
 def main():
     print("欢迎使用 美剧天堂 爬取脚本")
-    print("="*20)
-    print("魔幻/科幻：1\n灵异/惊悚：2\n都市/感情：3\n犯罪/历史：4\n选秀/综艺：5\n动漫/卡通：6")
-    print("="*20)
-    ftype = input('请输入需要爬取的类型的代号：')
-    start_url="http://www.meijutt.tv/file/list%s.html" %ftype
-    ourl = openurl.OpenUrl(start_url,'gb2312')
-    code,doc = ourl.run()    
+    #print("="*20)
+    #print("魔幻/科幻：1\n灵异/惊悚：2\n都市/感情：3\n犯罪/历史：4\n选秀/综艺：5\n动漫/卡通：6")
+    #print("="*20)
+    #ftype = input('请输入需要爬取的类型的代号：')
     pg_conn = pg_client.Mypostgres()
-    if code == 200:
-        selecter = etree.HTML(doc)
-        pages = selecter.xpath("//div[@class='page']/span/text()")[0].split()[0].split('/')[1]
-        firstpage_links = selecter.xpath("//a[@class='B font_14']/@href")
-        for firstpage_link in firstpage_links:
-            name,download_links = get_downlink(firstpage_link)
-            send_pg(pg_conn,[name, download_links])
-            time.sleep(0.5)
+    for ftype in range(1,7):
+        start_url="http://www.meijutt.tv/file/list{}.html".format(ftype)
+        ourl = openurl.OpenUrl(start_url,'gb2312')
+        code,doc = ourl.run()    
+        if code == 200:
+            selecter = etree.HTML(doc)
+            pages = selecter.xpath("//div[@class='page']/span/text()")[0].split()[0].split('/')[1]
+            firstpage_links = selecter.xpath("//a[@class='B font_14']/@href")
+            for firstpage_link in firstpage_links:
+                name,download_links = get_downlink(firstpage_link)
+                print(name)
+                if name != 'null':
+                    send_pg(pg_conn,[name, download_links])
+                time.sleep(0.5)
 
-        for page in range(2,int(pages)):
-            page_url = 'http://www.meijutt.tv/file/list%s_%s.html'%(ftype,page)
-            for link in page_link(page_url):
-                name,download_links = get_downlink(link)
-                if name != '' and download_links != '':
-                    send_pg(name, download_links)
+            for page in range(2,int(pages)):
+                page_url = 'http://www.meijutt.tv/file/list{0}_{1}.html'.format(ftype,page)
+                for link in page_link(page_url):
+                    name,download_links = get_downlink(link)
+                    print(name)
+                    if name != 'null':
+                        send_pg(pg_conn,[name, download_links])
                     time.sleep(0.5)
-    else:
-        print("[%s] error..."%start_url)
-    
+        else:
+            print("[{}] error...".format(start_url))
+
     print("Done.")
 if __name__ == '__main__':
     main()
