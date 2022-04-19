0ce273f1d25b200379442946b94cf5d37269c02b
kellanfan <icyfk1989@163.com>
2022-04-19 10:30:24 +0800

    优化函数

--- sometips/misc/exec_cmd.py
+++ sometips/misc/exec_cmd.py
@@ -6,7 +6,7 @@ def exec_cmd(cmd, remote_host=None):
     if remote_host is not None:
         # $ means to get the value in shell
         cmd = cmd.replace("$", "\$")
-        cmd = "ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -o ConnectTimeout=10 -o ConnectionAttempts=3 \
+        cmd = "ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile ~/.ssh/known_hosts' -o ConnectTimeout=10 -o ConnectionAttempts=3 \
             root@{0} \"{1}\"".format(remote_host, cmd)
 
     p = subprocess.Popen(cmd, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -16,10 +16,8 @@ def exec_cmd(cmd, remote_host=None):
     code = p.returncode
     out = out.strip()
     err = err.strip()
-
-    if err is not None:
-        result = {'code': 1, 'out': '', 'err':err}
+    if code != 0 and err:
+        result = {'code': code, 'out': '', 'err': err}
     else:
         result = {'code': code, 'out': out, 'err': err}
-        
     return result
